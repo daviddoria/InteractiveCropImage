98908bb4c890f4cfee2fecfcfb50dbb8b73352d2
David Doria <daviddoria@gmail.com>
2011-07-05 09:08:11 -0400

    License and small casting cleanup

--- Form.cxx
+++ Form.cxx
@@ -1,3 +1,21 @@
+/*=========================================================================
+ *
+ *  Copyright David Doria 2011 daviddoria@gmail.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
 #include "ui_Form.h"
 #include "Form.h"
 
@@ -82,12 +100,12 @@ void Form::BorderCallback(vtkObject *caller, unsigned long vtkNotUsed(eventId),
 
   // Setup the region to be extracted
   itk::Index<2> extractionStart;
-  extractionStart[0] = selectionXMin;
-  extractionStart[1] = selectionYMin;
+  extractionStart[0] = static_cast<unsigned int>(selectionXMin);
+  extractionStart[1] = static_cast<unsigned int>(selectionYMin);
 
   itk::Size<2> extractionSize;
-  extractionSize[0] = selectionXMax - selectionXMin;
-  extractionSize[1] = selectionYMax - selectionYMin;
+  extractionSize[0] = static_cast<unsigned int>(selectionXMax - selectionXMin);
+  extractionSize[1] = static_cast<unsigned int>(selectionYMax - selectionYMin);
 
   itk::ImageRegion<2> regionToExtract(extractionStart, extractionSize);
 
